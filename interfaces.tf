aaadad98ced8ee9ef52923f8c8d658b55215b0c0
morstad <morstad@us.ibm.com>
2017-02-12 12:35:23 -0600

    final updates

--- interfaces.tf
+++ interfaces.tf
@@ -12,16 +12,16 @@ variable "public_subnet" {
 
 variable "enable_dns_hostnames" {
   description = "Should be true if you want to use private DNS within the VPC."
-  default = true
+  default     = true
 }
 
 variable "enable_dns_support" {
   description = "Should be true if you want to use private DNS within the VPC."
-  default = true
+  default     = true
 }
 
 output "public_subnet_id" {
-  value = "${aws_subnet.public.id}"
+  value     = "${aws_subnet.public.id}"
   sensitive = true
 }
 
--- main.tf
+++ main.tf
@@ -10,20 +10,22 @@ resource "aws_vpc" "tfb" {
 
 resource "aws_internet_gateway" "tfb" {
   vpc_id = "${aws_vpc.tfb.id}"
+
   tags {
     Name = "${var.name}-igw"
   }
 }
 
 resource "aws_route" "internet_access" {
-  route_table_id = "${aws_vpc.tfb.main_route_table_id}"
+  route_table_id         = "${aws_vpc.tfb.main_route_table_id}"
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id = "${aws_internet_gateway.tfb.id}"
+  gateway_id             = "${aws_internet_gateway.tfb.id}"
 }
 
 resource "aws_subnet" "public" {
-  vpc_id = "${aws_vpc.tfb.id}"
+  vpc_id     = "${aws_vpc.tfb.id}"
   cidr_block = "${var.public_subnet}"
+
   tags {
     Name = "${var.name}-public"
   }
