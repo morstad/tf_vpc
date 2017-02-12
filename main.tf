94be030d04fc7ae8e54e9fcdc75eeb09d96e2adc
morstad <morstad@us.ibm.com>
2017-02-12 10:42:56 -0600

    fixed typo

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "tfb" {
-  cidr_block           = "${var.vpc.cidr}"
+  cidr_block           = "${var.cidr}"
   enable_dns_hostnames = "${var.enable_dns_hostnames}"
   enable_dns_support   = "${var.enable_dns_support}"
 
